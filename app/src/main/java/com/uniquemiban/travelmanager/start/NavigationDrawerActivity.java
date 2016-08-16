c1121d9e885285decaca1b8567a70fc7d903015a
Erik Arzumanyan <erikarzumanyan94@gmail.com>
2016-08-16 17:50:32 +0400

    Design, EatFragment, FragmentsBackStack

--- app/src/main/java/com/uniquemiban/travelmanager/start/NavigationDrawerActivity.java
+++ app/src/main/java/com/uniquemiban/travelmanager/start/NavigationDrawerActivity.java
@@ -1,6 +1,8 @@
 package com.uniquemiban.travelmanager.start;
 
 import android.content.Intent;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -10,10 +12,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.uniquemiban.travelmanager.eat.EatListFragment;
 import com.uniquemiban.travelmanager.utils.Constants;
 import com.uniquemiban.travelmanager.R;
 import com.uniquemiban.travelmanager.login.LoginActivity;
@@ -25,12 +31,25 @@ public class NavigationDrawerActivity extends AppCompatActivity
 
     private DrawerLayout mDrawer;
     private Toolbar mToolbar;
+    private int mWidth = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int uiOps = getWindow().getDecorView().getSystemUiVisibility();
+        uiOps |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        getWindow().getDecorView().setSystemUiVisibility(uiOps);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        mWidth = Math.min(width, height);
 
 //        //Check if google play services is up to date
 //        final int playServicesStatus = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
@@ -69,6 +88,10 @@ public class NavigationDrawerActivity extends AppCompatActivity
                     .add(R.id.fragment_container, fragmentSightsList, SightsListFragment.FRAGMENT_TAG)
                     .commit();
         }
+    }
+
+    public int getWidth() {
+        return mWidth;
     }
 
     public DrawerLayout getDrawer() {
@@ -112,11 +135,44 @@ public class NavigationDrawerActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
 
+        FragmentManager manager = getSupportFragmentManager();
+
         if (id == R.id.nav_sightseeing) {
+
+            Fragment fragment = manager.findFragmentByTag(SightsListFragment.FRAGMENT_TAG);
+
+            if(fragment == null){
+                fragment = new SightsListFragment();
+                manager.beginTransaction()
+                        .add(R.id.fragment_container, fragment, SightsListFragment.FRAGMENT_TAG)
+                        .addToBackStack(SightsListFragment.FRAGMENT_TAG)
+                        .commit();
+            }else {
+                manager.popBackStack(SightsListFragment.FRAGMENT_TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                manager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment, SightsListFragment.FRAGMENT_TAG)
+                        .addToBackStack(SightsListFragment.FRAGMENT_TAG)
+                        .commit();
+            }
 
         } else if (id == R.id.nav_sleeping) {
 
         } else if (id == R.id.nav_eating) {
+            Fragment fragment = manager.findFragmentByTag(EatListFragment.FRAGMENT_TAG);
+
+            if(fragment == null){
+                fragment = new EatListFragment();
+                manager.beginTransaction()
+                        .add(R.id.fragment_container, fragment, EatListFragment.FRAGMENT_TAG)
+                        .addToBackStack(EatListFragment.FRAGMENT_TAG)
+                        .commit();
+            }else {
+                manager.popBackStack(SightsListFragment.FRAGMENT_TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                manager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment, EatListFragment.FRAGMENT_TAG)
+                        .addToBackStack(EatListFragment.FRAGMENT_TAG)
+                        .commit();
+            }
 
         } else if (id == R.id.nav_tours) {
 
--- app/src/main/java/com/uniquemiban/travelmanager/utils/Constants.java
+++ app/src/main/java/com/uniquemiban/travelmanager/utils/Constants.java
@@ -3,6 +3,7 @@ package com.uniquemiban.travelmanager.utils;
 public class Constants {
 
     public static final String FIREBASE_SIGHTS = "Sights";
+    public static final String FIREBASE_EATS = "Eats";
 
     public static final String SHARED_PREFS = "shared_preferences";
 }
